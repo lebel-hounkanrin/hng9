36af6de3430d181c3fd6f67481d257dcd7e30238
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2022-10-29 16:20:36 +0100

    fix: change slackUsername fields

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const server = http.createServer(app);
 app.get("/", (req, res) => {
     res.json({
-        "SlackUsername": "Neilphillip",
+        "slackUsername": "Neilphillip",
         "backend": true,
         "age": 22,
         "bio": "My name's Lebel HOUNKANRIN. I'm Tech enthusiast, i'm junior software engineer. I'm a kind of adventurer"
